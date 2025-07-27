fcb51ad69bd15d98786a57eac77e79a5c1785095
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-07-27 22:17:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstChar(text) {
   // your code here
+	// Trim leading/trailing whitespace and return the first character, if any
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ' ') {
+      return str[i];
+    }
+  }
+  return '';
 }
 
 // Do not change the code below
